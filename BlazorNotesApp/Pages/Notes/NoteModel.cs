dc565820df6d452f12d65f335fc5cf18562bd95b
Tshibanda Patrick <tshibandap@gmail.com>
2020-11-16 22:46:26 +0100

    initial commit

---
+++ BlazorNotesApp/Pages/Notes/NoteModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BlazorNotesApp.Pages.Notes
+{
+    public class NoteModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string  Content { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
