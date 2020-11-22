060b73b98f651e28d72443f8a87eed6c7cd6a9b9
Tshibanda Patrick <tshibandap@gmail.com>
2020-11-22 14:58:11 +0100

    implement crud

--- BlazorNotesApp/Pages/Notes/NoteModel.cs
+++ BlazorNotesApp/Pages/Notes/NoteModel.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorNotesApp.Pages.Notes
 {
     public class NoteModel
     {
         public Guid Id { get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(20, ErrorMessage = "The title is too long")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Content is required")]
         public string  Content { get; set; }
         public DateTime Created { get; set; }
+        public bool ReadOnly { get; set; } = true;
     }
 }
